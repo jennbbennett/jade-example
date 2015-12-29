77ef778416fe59812466d93b2b41515d8d3be612
Jennifer Bennett <jennbbennett@hotmail.com>
2015-12-28 17:22:54 -0700

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var router = express.Router();
+
+var knex = require('./../db/knex');
+
+function Students() {
+  return knex('students');
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  Students().select().then(function(students) {
+    res.render('index', {students: students});
+  });
+});
+
+router.get('/enroll', function(req, res, next) {
+  res.render('enroll');
+});
+
+router.get('/:id', function(req, res, next) {
+  Students().where({
+    id: req.params.id,
+  }).then(function (student)
+    {res.render('show', {student:student[0]});
+  });
+});
+
+router.get('/edit/:id', function(req,res,next){
+    Students().where({
+      id: req.params.id,
+    }).then(function (student)
+      {res.render('edit', {student:student[0]});
+    });
+});
+
+router.post('/', function(req, res) {
+  Students().insert({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      date_of_birth: req.body.date_of_birth,
+    }, "id").then(function(id){
+      res.redirect('/');
+    });
+  });
+
+router.post('/edit/:id', function(req, res, next){
+  Students().where({
+    id:req.params.id,
+  }).update({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    date_of_birth: req.body.date_of_birth,
+  }, "id").then(function(id){
+    res.redirect('/' + req.params.id);
+  });
+});
+
+router.post('/delete/:id', function (req, res) {
+  Students().where({
+    id: req.params.id
+  }).del().then(function (result) {
+    res.redirect('/');
+  });
+});
+
+
+module.exports = router;
